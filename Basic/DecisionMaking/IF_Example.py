da2c5af1d223acc55d2efec2b1af6736b96b94ae
Pawan kumar <pawan431995@gmail.com>
2019-01-29 16:27:12 +0530

    Decision Making

---
+++ Basic/DecisionMaking/IF_Example.py
@@ -0,0 +1,12 @@
+# If Decission Making
+
+var1 = 100
+if var1:
+   print ("1 - Got a true expression value")
+   print (var1)
+
+var2 = None
+if var2:
+   print ("2 - Got a true expression value")
+   print (var2)
+print ("Good bye!")
